9e5cbdaa0fad49ac5cb7815d0927f3ac2dd4f592
santiç <santihermor50@gmail.com>
2023-08-16 18:44:02 -0300

    primer commit

---
+++ parcial-frontend3/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Card from './Components/Card'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className='App'>
+      <h1>Carga de estudiantes</h1>
+      <form></form>
+      <Card/>
+      </div>
+      
+    
+  )
+}
+
+export default App
